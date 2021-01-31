796e95a23a50be86fa39181c070ab332cf77e94f
Sarah Park <60310001+sarahyaepark@users.noreply.github.com>
2021-01-30 17:18:25 -0800

    Working V2: added search (#2)

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -1,3 +1,3 @@
 export { default as VendorList } from './vendorList';
 export { default as DropdownFilter } from './filter';
-export { default as SearchBox } from './search'
+export { default as SearchBox } from './search';
